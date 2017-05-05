126291156b8fbe71217b62bb76871e6fbb9e90d0
EricLeer <eric.leerssen@gmail.com>
2017-05-05 10:25:43 +0200

    implemented file searching and removing

--- clean_downloads.py
+++ clean_downloads.py
@@ -1,9 +1,23 @@
-import os
+from os import listdir, path, remove
+import datetime
+import shutil
 
-def fileNames(folder):
-    #findfiles in folder
-def cleanup(folder):
-    fileNames(folder)
+def cleanup(folder,cutoffdays, removeFiles):
+    filenames = listdir(folder)
+    for file in filenames:
+        filePath = folder + "\\" + file
+        fileTime = datetime.datetime.fromtimestamp(path.getmtime(filePath))
+        now = datetime.datetime.now() - datetime.timedelta(days=cutoffdays)
+        if fileTime<now:
+            print(filePath)
+            if removeFiles:
+                try:
+                    remove(filePath)
+                    print("file removed: " + filePath)
+                except OSError:
+                    shutil.rmtree(filePath)
+                    print("folder removed: " + filePath)
+
 if __name__ == "__main__":
-    folder = ""
-    cleanup(folder)
+    folder = r"C:\Users\ericl\Downloads"
+    cleanup(folder, 60, True)
